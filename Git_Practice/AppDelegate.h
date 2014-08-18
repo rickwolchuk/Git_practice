3de97bd98cad2ac6aee765d694fd2457792a5f07
Rick Wolchuk <rickwolchuk@gmail.com>
2014-08-18 14:49:09 -0500

    Initial Commit

---
+++ Git_Practice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Git_Practice
+//
+//  Created by Rick Wolchuk on 8/18/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git_Practice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Git_Practice
+//
+//  Created by Rick Wolchuk on 8/18/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
